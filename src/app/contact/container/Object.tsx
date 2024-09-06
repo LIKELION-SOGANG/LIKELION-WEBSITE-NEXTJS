7ac03c876636476d95b67b290e9beaf55c2d70f2
chlqhgus <bocurent@gmail.com>
2024-09-06 11:17:47 +0900

    Feat:containe에 links/object 컴포넌트를 추가했습니다.

---
+++ src/app/contact/container/Object.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Object() {
+    return <div>page</div>
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Object from './container/Object'
+import Links from './container/Links'
 
 export default function ContactPage() {
   return <div>page</div>
