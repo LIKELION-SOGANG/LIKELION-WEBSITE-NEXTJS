b78c3f9b967f1d3de6829066093f8238c9d995c1
SEOA <tjdk2002@gmail.com>
2024-09-10 00:39:53 +0900

    Feat: People Component 마크업 중간 구현 #11

---
+++ src/app/people/container/FirstSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FirstSection() {
+  return (
+    <div>
+      <div className="flex justify-center items-center w-full h-[100vh] bg-white">
+        <div className="font-pp text-black text-[4vw] capitalize m-auto italic">
+          Our People
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import FirstSection from './container/FirstSection'
+import SecondSection from './container/SecondSection'
 
 export default function PeoplePage() {
-  return <div>page</div>
+  return (
+    <div>
+      <FirstSection />
+      <SecondSection />
+    </div>
+  )
 }
