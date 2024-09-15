1ced4c06a87e3a81508da7dc74eddb1c5611653e
chlqhgus <bocurent@gmail.com>
2024-09-15 23:44:49 +0900

    Chore:media queries are completed

--- src/app/contact/container/Contact1.tsx
+++ src/app/contact/container/Contact1.tsx
@@ -3,28 +3,43 @@ import React from 'react'
 export default function Contact1() {
   return (
     <div>
-      <div className="relative w-full h-screen overflow-x-hidden">
+      <div className="relative w-full h-screen overflow-x-hidden overflow-y-hidden">
         <img
-          className=" absolute desktop:left-[11.1vw] tablet:left-[11.1vw] mobile:left-[50vw] w-[33vw] h-[33vw] top-[-10vw]"
+          className={`absolute
+            mobile:left-[35vw] w-[53vw] h-[52vw] top-[-20.3vw]
+            tablet:left-[12vw] tablet:w-[34vw] tablet:h-[34vw] tablet:top-[-10vw]
+            desktop:left-[12vw] desktop:w-[34vw] desktop:h-[34vw] desktop:top-[-10vw]  
+            `}
           src="/images/contact/obj1.png"
         />
         <img
-          className="absolute w-[45vw] h-[45vw] left-[66vw]"
+          className={`absolute 
+            mobile:w-[50vw] mobile:h-[50vw] mobile:mt-40 mobile:pt-6 mobile:left-[65vw]
+            tablet:w-[45vw] tablet:h-[43vw] tablet:mt-0 tablet:pt-0 tablet:left-[66vw]
+            desktop:w-[45vw] desktop:h-[43vw] desktop:mt-0 desktop:pt-0 desktop:left-[66vw] 
+            `}
           src="/images/contact/obj2.png"
         />
       </div>
-      <div className="text-center mt-[-24vw]">
-        <div className="text-[6rem] text-black font-pp font-thin">
+      <div className="text-center desktop:mt-[-24vw] tablet:mt-[-24vw] mobile:mt-[-50vh]">
+        <div className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[4rem] text-black font-pp font-thin">
           Connect With Us!
         </div>
       </div>
-      <div className="relative w-full h-[80vh] overflow-x-hidden">
+      <div className="relative w-full h-[80vh] overflow-x-hidden overflow-y-hidden">
         <img
-          className="absolute w-[31vw] h-[29vw] top-[-1vw]"
+          className={`absolute 
+            desktop:left-0 desktop:w-[31vw] desktop:h-[29vw] 
+            tablet:left-0 tablet:w-[31vw] tablet:h-[29vw]
+            mobile:left-[-10vw] mobile:w-[35vw] mobile:h-[34vw] top-[-1vw]`}
           src="/images/contact/obj3.png"
         />
         <img
-          className="absolute w-[31vw] h-[32vw] left-[58vw] top-[10vw]"
+          className={`absolute 
+          desktop:w-[31vw] desktop:h-[32vw] desktop:left-[58vw] desktop:top-[10vw]
+          tablet:w-[31vw] tablet:h-[32vw] tablet:left-[58vw] tablet:top-[10vw]
+          mobile:w-[70vw] mobile:h-[70vw] mobile:left-[50vw] mobile:top-[40vw] 
+          `}
           src="/images/contact/obj4.png"
         />
       </div>
--- src/app/contact/container/Contact2.tsx
+++ src/app/contact/container/Contact2.tsx
@@ -3,56 +3,56 @@ import React from 'react'
 export default function Contact2() {
   return (
     <div className="overflow-x-hidden">
-      <div className="desktop:px-80 ml-16 gap-16 tablet:px-80 ml-16 gap-16 mobile:px-20 ml-8 gap-32 flex flex-col ">
+      <div className="desktop:px-80 ml-16 gap-16 tablet:px-80 ml-16 gap-16 mobile:px-16 ml-4 gap-32 flex flex-col ">
         <div>
-          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[2rem] text-black font-pp">
+          <p className="desktop:text-[2.2rem] tablet:text-[2.5rem] mobile:text-[1.8rem] text-black font-pp">
             (instagram)
           </p>
           <a
             href="https://www.instagram.com/likelion_sg/"
-            className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[3.5rem] text-black font-pp font-normal group inline-block"
+            className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[2.8rem] text-black font-pp font-normal group inline-block"
             target="_blank">
             @likelion_sg ↘
             <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
           </a>
         </div>
         <div>
-          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[2rem] text-black font-pp">
+          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[1.8rem] text-black font-pp">
             (github)
           </p>
           <a
             href="https://github.com/LIKELION-SOGANG"
-            className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[3.5rem] text-black font-pp font-normal group inline-block"
+            className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[2.8rem] text-black font-pp font-normal group inline-block"
             target="_blank">
             likelionsg ↘︎
             <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
           </a>
         </div>
         <div>
-          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[2rem] text-black font-pp">
+          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[1.8rem] text-black font-pp">
             (email)
           </p>
-          <a className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[3.5rem] text-black font-pp font-normal group inline-block">
+          <a className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[2.8rem] text-black font-pp font-normal group inline-block">
             likelion_sg@gmail.com ↘︎
             <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
           </a>
         </div>
         <div>
-          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[2rem] text-black font-pp">
+          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[1.8rem] text-black font-pp">
             (kakaotalk)
           </p>
-          <a className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[3.5rem] text-black font-pp font-normal group inline-block">
+          <a className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[2.8rem] text-black font-pp font-normal group inline-block">
             inyoungjung ↘︎
             <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
           </a>
         </div>
         <div>
-          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[2rem] text-black font-pp">
+          <p className="desktop:text-[2.5rem] tablet:text-[2.5rem] mobile:text-[1.8rem] text-black font-pp">
             (likelion univ.)
           </p>
           <a
             href="https://likelion.university/"
-            className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[3.5rem] text-black font-pp font-normal group inline-block"
+            className="desktop:text-[6rem] tablet:text-[6rem] mobile:text-[2.8rem] text-black font-pp font-normal group inline-block"
             target="_blank">
             https://likelion.university/ ↘︎
             <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
