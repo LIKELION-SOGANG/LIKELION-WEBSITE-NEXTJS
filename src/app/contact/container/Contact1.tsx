997c415a11334b2e60e2954c54df4a61908f1aaf
chlqhgus <bocurent@gmail.com>
2024-09-15 16:08:34 +0900

    Feat:Underlining link completed

--- src/app/contact/container/Contact1.tsx
+++ src/app/contact/container/Contact1.tsx
@@ -18,51 +18,16 @@ export default function Contact1() {
           Connect With Us!
         </div>
       </div>
-      <div className="relative w-full h-screen overflow-x-hidden">
+      <div className="relative w-full h-[80vh] overflow-x-hidden">
         <img
-          className="absolute w-[43.75vw] h-[60vw] top-[-17vw]"
+          className="absolute w-[31vw] h-[29vw] top-[-1vw]"
           src="/images/contact/obj3.png"
         />
         <img
-          className="absolute w-[30vw] h-[31vw] left-[58vw] top-[10vw]"
+          className="absolute w-[31vw] h-[32vw] left-[58vw] top-[10vw]"
           src="/images/contact/obj4.png"
         />
       </div>
-      <div className="mt-[-10vw] px-80 ml-16 flex flex-col gap-16">
-        <div>
-          <p className="text-black text-[2.5rem] font-pp">(instagram)</p>
-          <a
-            href="https://www.instagram.com/likelion_sg/"
-            className="text-black text-[6rem] font-pp font-normal">
-            @likelion_sg ↘
-          </a>
-        </div>
-        <div>
-          <p className="text-black text-[2.5rem] font-pp">(github)</p>
-          <a className="text-black text-[6rem] font-pp font-normal">
-            likelionsg ↘︎
-          </a>
-        </div>
-        <div>
-          <p className="text-black text-[2.5rem] font-pp">(email)</p>
-          <a className="text-black text-[6rem] font-pp font-normal">
-            likelion_sg@gmail.com ↘︎
-          </a>
-        </div>
-        <div>
-          <p className="text-black text-[2.5rem] font-pp">(kakaotalk)</p>
-          <a className="text-black text-[6rem] font-pp font-normal">
-            inyoungjung ↘︎
-          </a>
-        </div>
-        <div>
-          <p className="text-black text-[2.5rem] font-pp">(likelion univ.)</p>
-          <a className="text-black text-[6rem] font-pp font-normal">
-            https://likelion.university/ ↘︎
-          </a>
-        </div>
-      </div>
-      <div className="p-80"></div>
     </div>
   )
 }
--- src/app/contact/container/Contact2.tsx
+++ src/app/contact/container/Contact2.tsx
@@ -2,6 +2,54 @@ import React from 'react'
 
 export default function Contact2() {
   return (
-    <h1></h1>
+    <div>
+      <div className="px-80 ml-16 flex flex-col gap-16">
+        <div>
+          <p className="text-black text-[2.5rem] font-pp">(instagram)</p>
+          <a
+            href="https://www.instagram.com/likelion_sg/"
+            className="text-black text-[6rem] font-pp font-normal group inline-block"
+            target="_blank">
+            @likelion_sg ↘
+            <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
+          </a>
+        </div>
+        <div>
+          <p className="text-black text-[2.5rem] font-pp">(github)</p>
+          <a
+            href="https://github.com/LIKELION-SOGANG"
+            className="text-black text-[6rem] font-pp font-normal group inline-block"
+            target="_blank">
+            likelionsg ↘︎
+            <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
+          </a>
+        </div>
+        <div>
+          <p className="text-black text-[2.5rem] font-pp">(email)</p>
+          <a className="text-black text-[6rem] font-pp font-normal group inline-block">
+            likelion_sg@gmail.com ↘︎
+            <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
+          </a>
+        </div>
+        <div>
+          <p className="text-black text-[2.5rem] font-pp">(kakaotalk)</p>
+          <a className="text-black text-[6rem] font-pp font-normal group inline-block">
+            inyoungjung ↘︎
+            <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
+          </a>
+        </div>
+        <div>
+          <p className="text-black text-[2.5rem] font-pp">(likelion univ.)</p>
+          <a
+            href="https://likelion.university/"
+            className="text-black text-[6rem] font-pp font-normal group inline-block"
+            target="_blank">
+            https://likelion.university/ ↘︎
+            <div className=" h-[3px] -mt-6 group-hover:animate-scaleLeft group-hover:bg-black "></div>
+          </a>
+        </div>
+      </div>
+      <div className="p-80"></div>
+    </div>
   )
 }
