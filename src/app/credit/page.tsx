81447e95a0409fd5e52e7fbc533618f569c5e35a
chlqhgus <bocurent@gmail.com>
2024-09-20 23:00:10 +0900

    Feat: 브랜치 삭제 #15

--- src/app/credit/page.tsx
+++ src/app/credit/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export default function CreditPage() {
-  return <div>page</div>
+  return (
+    <main>
+      <div className="bg-black w-full h-screen">
+        <div className="justify-center content-center">
+          <h3 className="font-pp text-white text-center text-[3rem] pt-56">
+            CREDITS
+          </h3>
+        </div>
+      </div>
+    </main>
+  )
 }
