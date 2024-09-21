45f1e7df8988383a575d41a7e22883b5eda11c86
shu07002@gmail.com <shu07002@gmail.com>
2024-09-21 23:59:55 +0900

    Feat: Recruit1 페이지 스크롤 로티 추가 #33

--- src/app/recruit/ScrollLottie.tsx
+++ src/app/recruit/ScrollLottie.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Lottie from 'lottie-react'
+import ScrollAnimation from '../../../public/lottie/animation.json'
 
 const ScrollLottie = () => {
-  return <div>스크롤 Lottie</div>
+  return (
+    <Lottie
+      animationData={ScrollAnimation}
+      loop={true}
+    />
+  )
 }
 
 export default ScrollLottie
