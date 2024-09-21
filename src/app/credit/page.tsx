4e3760ac256a6f7d662e94b80d38dea28587fa05
chlqhgus <bocurent@gmail.com>
2024-09-21 16:37:14 +0900

    Design: 반응형 스타일 구현 #15

--- src/app/credit/page.tsx
+++ src/app/credit/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 export default function CreditPage() {
@@ -6,65 +6,99 @@ export default function CreditPage() {
     <main>
       <div className="bg-black w-full h-full">
         <div className="flex flex-col justify-center font-pp text-white">
-          <h3 className=" text-center text-[3rem] pt-64">CREDITS</h3>
-          <ul className="flex flex-col gap-20 py-[4vw] px-[20vw] ml-[11vw]">
-            <li className="flex justify-between ml-24 pl-4">
-              <p className="text-right text-[1.9rem] pr-10 w-1/3">Design</p>
-              <p className="text-[1.9rem] leading-10 w-2/3">
+          <h3
+            className={`text-center text-[2.5rem] pt-64
+            mobile:text-[2.5rem]
+            tablet:text-[3rem]
+            desktop:text-[3rem]`}>
+            CREDITS
+          </h3>
+
+          <ul
+            className={`flex flex-col gap-24 py-[10vw] mb-[18vw]
+              mobile:ml-[5vw] mobile:mb-[7vw]
+              tablet:mb-0 tablet:py-[6vw]
+            desktop:py-[4vw] desktop:px-[8vw] desktop:ml-[13vw] desktop:mb-8 desktop:gap-20`}>
+            {/* Design Section */}
+            <li
+              className={`flex justify-between items-start gap-10 pl-[14vw] 
+              desktop:ml-24 desktop:pl-4`}>
+              <p
+                className={`text-right w-[35%] text-[1.6rem] whitespace-nowrap 
+                desktop:text-[1.9rem]`}>
+                Design
+              </p>
+              <p
+                className={`leading-10 flex-grow text-[1.6rem] desktop:text-[1.9rem]`}>
                 Sunmyeong Lee
                 <br />
                 Koeun Jeong
               </p>
             </li>
 
-            <li className="flex justify-between ml-24 pl-4">
-              <p className="text-right text-[1.9rem] pr-10 w-1/3 whitespace-nowrap">
+            {/* Front-End Develop Section */}
+            <li
+              className={`flex justify-between items-start gap-10 pl-[14vw] 
+              desktop:ml-24 desktop:pl-4`}>
+              <p
+                className={`text-right w-[35%] text-[1.6rem] 
+                desktop:text-[1.9rem] desktop:whitespace-nowrap `}>
                 Front-End Develop
               </p>
-              <p className="text-[1.9rem] leading-10 w-2/3">
+              <p
+                className={`leading-10 flex-grow text-[1.6rem] desktop:text-[1.9rem]`}>
                 Inyoung Jung
                 <br />
-                Sunmyeong lee
+                Sunmyeong Lee
                 <br />
-                Koeun jeong
+                Koeun Jeong
                 <br />
-                Sehwan jang
+                Sehwan Jang
               </p>
             </li>
 
-            <li className="flex justify-between ml-24 pl-4">
-              <p className="text-right text-[1.9rem] pr-10 w-1/3 whitespace-nowrap">
+            {/* Back-End Develop Section */}
+            <li
+              className={`flex justify-between items-start gap-10 pl-[14vw] 
+              desktop:ml-24 desktop:pl-4`}>
+              <p
+                className={`text-right w-[35%] text-[1.6rem]
+                desktop:text-[1.9rem] desktop:whitespace-nowrap`}>
                 Back-End Develop
               </p>
-              <p className="text-[1.9rem] leading-10 w-2/3">
+              <p
+                className={`leading-10 flex-grow text-[1.6rem] desktop:text-[1.9rem]`}>
                 Yuyi Kim
                 <br />
                 Jeongyeon Lim
               </p>
             </li>
           </ul>
+
           <p className="text-center text-[2.7rem]">✽</p>
           <div>
-            <div className="flex justify-center gap-8 pt-12 mt-4">
+            <div className="flex justify-center pt-12 mt-4">
               <Image
                 src="/images/credit/sgLogo-Black.png"
                 alt="Sogang University logo"
                 width={60}
-                height={40}
+                height={50}
                 layout="fixed"
                 style={{ objectFit: 'contain' }}
               />
-              <p className="text-[4.5rem]">
+              <p className="text-[3rem] tablet:text-[4.5rem] desktop:text-[4.5rem]">
                 Like<span className="italic">lion</span> So
                 <span className="italic">gang</span>
               </p>
             </div>
-            <p className="text-center text-[1.2rem] pt-12">
+            <p
+              className="text-center text-[1.1rem] pt-14 px-[10vw] 
+              tablet:px-[22vw] tablet:text-[1.2rem]
+            desktop:text-[1.2rem] desktop:px-[30vw]">
               Likelion Sogang, a leading IT startup club at Sogang University,
-              empowers members to bring their ideas to life,
-              <br />
-              fostering innovation, and achieving notable success with the
-              motto, "Let's actualize our ideas with our own hands!"
+              empowers members to bring their ideas to life, fostering
+              innovation, and achieving notable success with the motto, "Let's
+              actualize our ideas with our own hands!"
             </p>
             <p className="text-center text-[1.2rem] pt-12 pb-40">
               © Likelion Sogang. All Rights Reserved.
