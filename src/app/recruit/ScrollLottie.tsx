06b1bc1cc1fb1997e8ae0b673767e05fb265d566
shu07002@gmail.com <shu07002@gmail.com>
2024-09-22 00:07:10 +0900

    Feat: Recruit1 페이지 스크롤 로티 반응형 추가 #33

--- src/app/recruit/ScrollLottie.tsx
+++ src/app/recruit/ScrollLottie.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Lottie from 'lottie-react'
-import ScrollAnimation from '../../../public/lottie/animation.json'
+import ScrollAnimationInDesktop from '../../../public/lottie/animationDesktop.json'
+import ScrollAnimationInMobile from '../../../public/lottie/animationMobile.json'
 
 const ScrollLottie = () => {
   return (
-    <Lottie
-      animationData={ScrollAnimation}
-      loop={true}
-    />
+    <div>
+      <Lottie
+        className="max-tablet:hidden"
+        animationData={ScrollAnimationInDesktop}
+        loop={true}
+      />
+      <Lottie
+        className="tablet:hidden"
+        animationData={ScrollAnimationInMobile}
+        loop={true}
+      />
+    </div>
   )
 }
 
