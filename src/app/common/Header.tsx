79319f11f9711a6038fac4e8bc5197775aa94590
inyoung <rmdnps10@gmail.com>
2024-10-03 19:21:03 +0900

    Feat: `Header` implementation complete #27

--- src/app/common/Header.tsx
+++ src/app/common/Header.tsx
@@ -4,13 +4,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState, useEffect } from 'react'
 
+//
+//
+//
+
 function Header() {
-  const [isShowHeader, setIsShowHeader] = useState(true) // 헤더 표시 여부
-  const [lastScrollY, setLastScrollY] = useState(0) // 마지막 스크롤 위치
+  const [isShowHeader, setIsShowHeader] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
   const [isShowMobileMenu, setIsShowMobileMenu] = useState(false)
 
-  const handleMobileMenuClick = () => {
+  const handleMobileHamburgerClick = () => {
     setIsShowMobileMenu(true)
+  }
+
+  const handleMobileCloseMenuClick = () => {
+    setIsShowMobileMenu(false)
   }
 
   useEffect(() => {
@@ -73,46 +81,57 @@ function Header() {
         </nav>
         {/* 모바일 */}
         {/* 햄버거 버튼 */}
-        <button className="block ml-auto tablet:hidden">
+        <button
+          className="block ml-auto tablet:hidden py-[1rem] pl-[1rem] "
+          onClick={handleMobileHamburgerClick}>
           <Image
             src={'/icon/button/hamburger-white.svg'}
             width={20}
             height={20}
             alt="메뉴 버튼"
-            onClick={handleMobileMenuClick}
           />
         </button>
       </header>
-      {/* 메뉴 리스트 */}
+      {/* 모바일 메뉴 리스트 - on/off*/}
       <section
-        className={`fixed top-0 w-full h-[100vh] bg-black z-${HEADER} transform transition-transform duration-500 ${
+        className={`fixed top-0 w-full h-[100vh] bg-black z-${HEADER} pt-[5rem] transform transition-transform duration-500 ${
           isShowMobileMenu ? 'translate-y-0' : '-translate-y-full'
         }`}>
+        <button
+          onClick={handleMobileCloseMenuClick}
+          className="absolute top-[2rem] right-[1.5rem] p-[1rem]">
+          <Image
+            src={'/icon/button/closeX-white.svg'}
+            width={25}
+            height={25}
+            alt="메뉴 닫기 버튼"
+          />
+        </button>
         <nav>
           <ul className="font-pp text-[3.2rem] font-[300]">
             <Link
               href="/about"
-              className="px-[3rem] py-[1.1rem] block">
+              className="px-[3rem] py-[1.1rem] block border-b border-white">
               About
             </Link>
             <Link
               href="/people"
-              className="px-[3rem] py-[1.1rem] block">
+              className="px-[3rem] py-[1.1rem] block border-b border-white">
               People
             </Link>
             <Link
               href="/project "
-              className="px-[3rem] py-[1.1rem] block">
+              className="px-[3rem] py-[1.1rem] block border-b border-white">
               Project
             </Link>
             <Link
               href="/recruit"
-              className="px-[3rem] py-[1.1rem] block">
+              className="px-[3rem] py-[1.1rem] block border-b border-white">
               Recruit
             </Link>
             <Link
               href="/contact"
-              className="px-[3rem] py-[1.1rem] block">
+              className="px-[3rem] py-[1.1rem] block border-b border-white">
               Contact
             </Link>
           </ul>
