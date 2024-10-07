46635757e11b863e3c73aeff8fdeb8cb4ba135bf
inyoung <rmdnps10@gmail.com>
2024-10-07 13:45:05 +0900

    Style: complete `Footer` UI #27

---
+++ src/app/component/Footer.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+
+//
+//
+//
+
+export default function Footer() {
+  return (
+    <footer className="w-full px-[2.5rem] py-[1.45rem] flex justify-between font-pp text-[1.5rem]">
+      <section className="flex gap-[2.5rem] items-center">
+        <div>
+          <p>Instagram.</p>
+          <p>@likelion_sg</p>
+        </div>
+        <div>
+          <p>Email</p>
+          <p>likelion_sg@gmail.com</p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <p className="text-right underline">
+            <Link href={'/credit'}>↖︎ Credits</Link>
+          </p>
+          <p>© Likelion Sogang. All Rights Reserved.</p>
+        </div>
+      </section>
+    </footer>
+  )
+}
--- src/style/zIndex.ts
+++ src/style/zIndex.ts
@@ -5,4 +5,4 @@ export const MOBILE_MENU_LIST = 99
 
 // 각 페이지별 최상단 <main>의 z-index
 
-export const PAGE = -1
+export const PAGE = 1
